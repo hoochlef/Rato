228b9cc772875a276915157cd7cf2de33e81f1bc
aminehochlef <amin.hochlef@gmail.com>
2025-02-28 13:41:09 +0100

    added cors enables origins

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,7 +1,19 @@
+from typing import Annotated, Any
+from pydantic import AnyUrl, BeforeValidator
 from pydantic_settings import BaseSettings
 
+# function to parse cors urls
+def parse_cors(v: Any) -> list[str] | str:
+    if isinstance(v, str) and not v.startswith("["):
+        return [i.strip() for i in v.split(",")]
+    elif isinstance(v, list | str):
+        return v
+    raise ValueError(v)
 
 class Settings(BaseSettings):
+    class Config:
+            env_file = ".env"
+
     database_hostname: str
     database_port: str
     database_password: str
@@ -11,8 +23,15 @@ class Settings(BaseSettings):
     algorithm: str
     access_token_expire_minutes: int
 
-    class Config:
-        env_file = ".env"
+    ##### Parsing cors allowed urls from .env
+    backend_cors_origins: Annotated[
+        list[AnyUrl] | str, BeforeValidator(parse_cors)
+    ] = []
+
+    @property
+    def all_cors_origins(self) -> list[str]:
+        return [str(origin).rstrip("/") for origin in self.backend_cors_origins]
+    #####
 
 
 settings = Settings()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routes import businesses, categories, users, reviews, login, vote
+from .config import settings
 
 app = FastAPI()
+
+# Set all CORS enabled origins
+if settings.all_cors_origins:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.all_cors_origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 # from .database import create_tables
 # Create tables automatically on startup, on_event is deprecated there's another way using
@@ -19,3 +30,8 @@ app.include_router(reviews.router)
 app.include_router(categories.router)
 app.include_router(login.router)
 app.include_router(vote.router)
+
+
+@app.get('/')
+def root():
+    return {"message": "yes"}
