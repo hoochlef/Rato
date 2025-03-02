7056680ce7febce7dba659b4cc53cb8e33264104
aminehochlef <amin.hochlef@gmail.com>
2025-03-02 17:55:48 +0100

    test commit --not-important

--- backend/app/main.py
+++ backend/app/main.py
@@ -40,13 +40,6 @@ if settings.all_cors_origins:
         allow_headers=["*"],
     )
 
-# from .database import create_tables
-# Create tables automatically on startup, on_event is deprecated there's another way using
-# lifespan events in fastAPI, for now I will stick to creating tables manually
-# @app.on_event("startup")
-# def on_startup():
-#     create_tables()
-
 @app.get('/')
 def root():
-    return {"message": "yess"}
+    return {"message": "Access /docs to see the API documentation"}
