ac872472302745fc9a961df024901dc1364e3c59
aminehochlef <amin.hochlef@gmail.com>
2025-03-02 17:19:24 +0100

    added automatic alembic migration on app startup to avoid empty database in prod

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,34 @@
+import logging
+import os
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from .api.main import api_router
 from .core.config import settings
 
-app = FastAPI()
+from alembic import command
+from alembic.config import Config
+
+log = logging.getLogger("uvicorn")
+
+# Add this function to run migrations
+def run_migrations():
+    alembic_cfg = Config(os.path.join(os.path.dirname(__file__), '../alembic.ini'))
+    command.upgrade(alembic_cfg, "head")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    log.info("Running migrations at startup...")
+    run_migrations()
+    yield
+    log.info("Application shutdown...")
+
+
+app = FastAPI(lifespan=lifespan)
+
+# Include the routes
+app.include_router(api_router)
 
 # Set all CORS enabled origins
 if settings.all_cors_origins:
@@ -23,10 +47,6 @@ if settings.all_cors_origins:
 # def on_startup():
 #     create_tables()
 
-# Include the routes
-app.include_router(api_router)
-
-
 @app.get('/')
 def root():
-    return {"message": "yes"}
+    return {"message": "yess"}
